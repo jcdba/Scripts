1125737eba9a76ba701d064a3107117827df4752
Junior Moraes <108875243+jcdba@users.noreply.github.com>
2022-09-10 12:42:32 -0300

    Add files via upload

---
+++ kill_All_Processes.sql
@@ -0,0 +1,14 @@
+DECLARE @query VARCHAR(MAX) = ''
+
+SELECT
+    @query = COALESCE(@query, ',') + 'KILL ' + CONVERT(VARCHAR, spid) + '; '
+FROM
+    master..sysprocesses
+WHERE
+    dbid = DB_ID('ab_renfix') -- Nome do database
+    AND dbid > 4 -- Não eliminar sessões em databases de sistema
+    AND spid <> '331' -- Não eliminar a sua própria sessão
+
+
+IF (LEN(@query) > 0)
+    EXEC(@query)
