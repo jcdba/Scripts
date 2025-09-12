837e8f8a10c2e893447b62cca4fb00af7b9e6305
Junior Moraes <108875243+jcdba@users.noreply.github.com>
2025-09-12 12:23:25 -0300

    Update kill_All_Processes.sql

--- kill_All_Processes.sql
+++ kill_All_Processes.sql
@@ -1,14 +1,17 @@
 DECLARE @query VARCHAR(MAX) = ''
 
 SELECT
-    @query = COALESCE(@query, ',') + 'KILL ' + CONVERT(VARCHAR, spid) + '; '
-FROM
-    master..sysprocesses
-WHERE
-    dbid = DB_ID('ab_renfix') -- Nome do database
-    AND dbid > 4 -- Não eliminar sessões em databases de sistema
-    AND spid <> '331' -- Não eliminar a sua própria sessão
+    @query = COALESCE(@query, ',') + 'KILL ' + CONVERT(VARCHAR, r.session_id) + '; '
+FROM sys.dm_exec_requests r
+JOIN sys.dm_exec_sessions s ON r.session_id = s.session_id
+CROSS APPLY sys.dm_exec_sql_text(r.sql_handle) t
+WHERE 
+	r.database_id = DB_ID('DW') -- FILTRA BANCO
+	AND s.login_name = 'automacao' -- FILTRA USUARIO
+	AND DATEDIFF(MINUTE, r.start_time, GETDATE()) > 10 -- FILTRA TEMPO DE EXECUÃ‡ÃƒO EX: +10 MINUTOS
+ORDER BY r.start_time;
 
 
 IF (LEN(@query) > 0)
     EXEC(@query)
+
